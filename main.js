84c172c795a8dc6e3b277da08c3e31d348d5a7b1
Carlos Nah <cnah27@gmail.com>
2019-09-12 13:31:26 +0000

    Refactor the editContact Method

--- main.js
+++ main.js
@@ -53,87 +53,101 @@ const addContactNames = (first, last, contact) => {
     contactNames.push({ firstname: first.trim(), lastname: last.trim(), contact: contact.trim() });
 }
 
-const editContactName = (pos, first, last, contacts) => {
+const editContactName = (pos, first, last, con) => {
     for (let i = 0; i < contactNames.length; i++) {
         //console.log(parseInt(numPosition, 10) === i, i)
         if (parseInt(pos, 10) - 1 === i) {
+            
             contactNames[i].firstname = first;
             contactNames[i].lastname = last;
-            contactNames[i].contact = contacts;
+            contactNames[i].contact = con;
+
+
         }
     }
 
-    }
+}
 
 
-    // Add functionalities
-    addButton.addEventListener("click", function () {
-        addContainer.style.display = "block";
-        editContainer.style.display = "none";
-        deleteContainer.style.display = "none";
-    })
+// Add functionalities
+addButton.addEventListener("click", function () {
+    addContainer.style.display = "block";
+    editContainer.style.display = "none";
+    deleteContainer.style.display = "none";
+})
 
-    addName.addEventListener("click", function () {
-        const firstname = document.querySelector("#firstname");
-        const lastname = document.querySelector("#lastname");
-        const contact = document.querySelector("#contact");
-        addContactNames(firstname.value, lastname.value, contact.value);
-        addContainer.style.display = "none";
-        console.log(contactNames);
-        displayNames();
-        firstname.value = null;
-        lastname.value = null;
-        contact.value = null;
-
-    });
-
-
-    // Edit functionalities
-    editButton.addEventListener("click", function () {
-        editContainer.style.display = "block";
-        addContainer.style.display = "none";
-        deleteContainer.style.display = "none";
-    })
-
-    editName.addEventListener("click", function () {
-        const editPosition = document.querySelector("#edit-position");
-        const editFirstName = document.querySelector("#edit-firstname");
-        const editLastName = document.querySelector("#edit-lastname");
-        const editContact = document.querySelector("#editContact");
-        //addContactNames(firstname.value, lastname.value, contact.value);
-        editContainer.style.display = "none";
-        console.log(editContact.value);
-        let numPosition = editPosition.value;
-        editContactName(numPosition, editFirstName.value, editLastName.value, editContact.value);
-        console.log(contactNames)
-        displayNames();
-        editPosition.value = null;
-        editFirstName.value = null;
-        editLastName.value = null;
-        editContact.value = null;
-
-    });
-
-    // Delete functionality
-    deleteButton.addEventListener("click", function () {
-        deleteContainer.style.display = "block";
-        addContainer.style.display = "none";
-        editContainer.style.display = "none";
-    });
-
-    deleteName.addEventListener("click", function () {
-        const deletePosition = document.querySelector("#delete-position");
-        deleteContactName(deletePosition.value);
-        deleteContainer.style.display = "none";
-        displayNames();
-        deletePosition.value = "";
-    })
-
-
-
-
-
-
+addName.addEventListener("click", function () {
+    const firstname = document.querySelector("#firstname");
+    const lastname = document.querySelector("#lastname");
+    const contact = document.querySelector("#contact");
+    addContactNames(firstname.value, lastname.value, contact.value);
+    addContainer.style.display = "none";
+    console.log(contactNames);
     displayNames();
+    firstname.value = null;
+    lastname.value = null;
+    contact.value = null;
+
+});
+
+
+// Edit functionalities
+editButton.addEventListener("click", function () {
+    editContainer.style.display = "block";
+    addContainer.style.display = "none";
+    deleteContainer.style.display = "none";
+})
+
+editName.addEventListener("click", function () {
+    let editPosition = document.querySelector("#edit-position");
+    let editFirstName = document.querySelector("#edit-firstname");
+    let editLastName = document.querySelector("#edit-lastname");
+    let editContact = document.querySelector("#editContact");
+    //addContactNames(firstname.value, lastname.value, contact.value);
+    editContainer.style.display = "none";
+    console.log(editContact.value);
+    let numPosition = editPosition.value;
+    if (editFirstName.value === "") {
+        editFirstName.value = contactNames[numPosition-1].firstname;
+    } 
+    if (editLastName.value === "") {
+        editLastName.value = contactNames[numPosition-1].lastname;
+    } 
+    if (editContact.value === "") {
+        editContact.value = contactNames[numPosition-1].contact;
+        console.log(editContact.value)
+    }
+    console.log(numPosition)
+    editContactName(numPosition, editFirstName.value, editLastName.value, editContact.value);
+    console.log(contactNames)
+    displayNames();
+    editPosition.value = null;
+    editFirstName.value = null;
+    editLastName.value = null;
+    editContact.value = null;
+
+});
+
+// Delete functionality
+deleteButton.addEventListener("click", function () {
+    deleteContainer.style.display = "block";
+    addContainer.style.display = "none";
+    editContainer.style.display = "none";
+});
+
+deleteName.addEventListener("click", function () {
+    const deletePosition = document.querySelector("#delete-position");
+    deleteContactName(deletePosition.value);
+    deleteContainer.style.display = "none";
+    displayNames();
+    deletePosition.value = "";
+})
+
+
+
+
+
+
+displayNames();
 
 
